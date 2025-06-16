88cf5ce9dbe646df24066ba72a3371702a1bae4f
mohidzafar <mohidzafar42@gmail.com>
2025-06-16 17:09:19 +0500

    updated navbar

--- src/services/services.js
+++ src/services/services.js
@@ -1,3 +1,22 @@
-document.getElementById('darkToggle').addEventListener('change', function () {
-  document.body.classList.toggle('dark');
-});
+  // Dark mode toggle (your working code)
+  document.getElementById('darkToggle').addEventListener('change', function () {
+    document.body.classList.toggle('dark');
+  });
+
+    const navbar = document.getElementById('navbar');
+
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    if (scrollY > 0 && scrollY < 0) {
+      navbar.classList.add('hide');
+      navbar.classList.remove('show-shadow');
+    } else {
+      navbar.classList.remove('hide');
+      if (scrollY > 300) {
+        navbar.classList.add('show-shadow');
+      } else {
+        navbar.classList.remove('show-shadow');
+      }
+    }
+  });
